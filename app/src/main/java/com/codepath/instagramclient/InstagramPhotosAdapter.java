81fe41a2218e6e611c1df4467aa7250837947622
Pritesh Shah <pritesh.shah@magnet.com>
2015-02-07 23:11:16 -0800

    Implemented view-holder pattern to improve performance

--- app/src/main/java/com/codepath/instagramclient/InstagramPhotosAdapter.java
+++ app/src/main/java/com/codepath/instagramclient/InstagramPhotosAdapter.java
@@ -14,26 +14,37 @@ import java.util.List;
 
 public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
 
+    // View lookup cache
+    private static class ViewHolder {
+        TextView caption;
+        ImageView photo;
+    }
+
     public InstagramPhotosAdapter(Context context, List<InstagramPhoto> objects) {
-        super(context, android.R.layout.simple_list_item_1, objects);
+        super(context, R.layout.item_photo, objects);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
+        // Get the data item for this position
         InstagramPhoto photo = getItem(position);
-
+        // Check if an existing view is being reused, otherwise inflate the view
+        ViewHolder viewHolder; // view lookup cache stored in tag
         if (convertView == null) {
+            viewHolder = new ViewHolder();
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_photo, parent, false);
+            viewHolder.caption = (TextView) convertView.findViewById(R.id.tvCaption);
+            viewHolder.photo = (ImageView) convertView.findViewById(R.id.ivPhoto);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        TextView tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
-        ImageView ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
-        tvCaption.setText(photo.getCaption());
-        ivPhoto.setImageResource(0);
-
-        Picasso.with(getContext()).load(photo.getImageUrl()).into(ivPhoto);
-
+        // Populate the data into the template view using the data object
+        viewHolder.caption.setText(photo.getCaption());
+        viewHolder.photo.setImageResource(0);
+        Picasso.with(getContext()).load(photo.getImageUrl()).into(viewHolder.photo);
+        // Return the completed view to render on screen
         return convertView;
     }
 }
